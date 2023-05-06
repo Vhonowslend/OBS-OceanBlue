fc97945b01de8470c165621313885457a61edc36
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2023-05-06 15:22:18 +0200

    More flexibility for building

---
+++ scripts/distrib.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const sass = require('sass');
+const path = require('path');
+
+const targetPath = path.join(process.cwd(), "distrib");
+const result = sass.compile(path.join(process.cwd(), "source", "main.scss"), { style: "compressed" });
+
+if (!fs.existsSync(targetPath))
+    fs.mkdirSync(path.join(targetPath), { recursive: true });
+fs.writeFileSync(path.join(targetPath, "OceanBlue.qss"), result.css, { flag: "w+" });
+fs.cpSync(path.join(process.cwd(), "data"), path.join(targetPath, "OceanBlue"), { force: true, recursive: true, preserveTimestamps: true })
